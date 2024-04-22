87db6d1f51e00d06a324ca9e4f646266b4103213
a.m <126760638+Kryzexx@users.noreply.github.com>
2024-04-22 12:11:22 +0400

    Update app.js

--- app.js
+++ app.js
@@ -6,3 +6,37 @@ document.querySelector('.eye-svg').addEventListener('click', () => {
     }
     else { password.type = 'password' }
 })
+
+document.querySelector('.reg-btn').addEventListener('click', () => {
+    if(document.querySelector('#pass1').value != document.querySelector('#pass2').value) {
+        return DisplayError('პაროლები არ ემთხვევა')
+    }
+    if(document.querySelector('.inputmail').value.includes("@") == false) {
+        return DisplayError('არასწორი მეილი')
+    }
+})
+
+
+/*let test = 2
+
+if(test == 1) {
+    document.querySelector('.login').style.display = 'flex'
+    document.querySelector('.register').style.display = 'none'
+}
+else if (test == 2) {
+    document.querySelector('.login').style.display = 'none'
+    document.querySelector('.register').style.display = 'flex' ------- გამოიყენე თუ დაგჭირდა
+}
+else {
+    document.querySelector('.login').style.display = 'none'
+    document.querySelector('.register').style.display = 'none'
+} */
+
+function DisplayError(errormsg) {
+    document.querySelector('.error').style.display = 'flex'
+    document.querySelector('.error-text span').textContent = errormsg
+    setTimeout(() => {
+        document.querySelector('.error').style.display = 'none'
+        document.querySelector('.error-text span').textContent = ''
+    }, 2500);
+}
