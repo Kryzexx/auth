0b3fd3f2c76eccc6b2a4c2fe8fe256289d325939
a.m <126760638+Kryzexx@users.noreply.github.com>
2024-04-23 17:16:46 +0400

    Update app.js

--- app.js
+++ app.js
@@ -16,6 +16,30 @@ document.querySelector('.reg-btn').addEventListener('click', () => {
     }
 })
 
+function downloadFile(file) {
+  // Create a link and set the URL using `createObjectURL`
+  const link = document.createElement("a");
+  link.style.display = "none";
+  link.href = URL.createObjectURL(file);
+  link.download = file.name;
+
+  // It needs to be added to the DOM so it can be clicked
+  document.body.appendChild(link);
+  link.click();
+
+  // To make this work on Firefox we need to wait
+  // a little while before removing it.
+  setTimeout(() => {
+    URL.revokeObjectURL(link.href);
+    link.parentNode.removeChild(link);
+  }, 0);
+}
+
+// Dynamically create a File
+const myFile = new File([`${new Date()}: Meow!`], "my-cat.txt");
+
+// Download it using our function
+downloadFile(myFile);
 
 /*let test = 2
 
