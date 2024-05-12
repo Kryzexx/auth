e1df5f524867a9eef7b4d8baa66013fc1a03b37c
a.m <126760638+Kryzexx@users.noreply.github.com>
2024-05-12 15:55:33 +0400

    new js

--- app.js
+++ app.js
@@ -8,29 +8,41 @@ document.querySelector('.eye-svg').addEventListener('click', () => {
 })
 
 document.querySelector('.reg-btn').addEventListener('click', () => {
+    if(document.querySelector('#pass1').value.length < 6) {
+        return DisplayError('პაროლი უნდა იყოს მინიმუმ 6 ასო')
+    }
     if(document.querySelector('#pass1').value != document.querySelector('#pass2').value) {
         return DisplayError('პაროლები არ ემთხვევა')
     }
     if(document.querySelector('.inputmail').value.includes("@") == false) {
         return DisplayError('არასწორი მეილი')
     }
+    else {
+        // - success, ამას რო მიიღებ მერე უკვე სქესი და რეებიცაა, ამ CEF'ს destroybrowser-ი უქენი
+        cef.emit('pwd:sucreg')
+    }
 })
 
-let test = 2
+/* -- თუ უკვე რეგისტრირებულია, Response სახით დააბრუნე 1 ხოლო თუ არაა დააბრუნე 2;
+1 = Log In
+2 = Register */
+cef.on('pwd:login', (response) => { 
+    if(response == 1) {
+        document.querySelector('.login').style.display = 'flex'
+        document.querySelector('.register').style.display = 'none'
+    }
+    else if (response == 2) {
+        document.querySelector('.login').style.display = 'none'
+        document.querySelector('.register').style.display = 'flex'
+    }
+    else {
+        document.querySelector('.login').style.display = 'none'
+        document.querySelector('.register').style.display = 'none'
+    } 
+    
+}) 
 
-if(test == 1) {
-    document.querySelector('.login').style.display = 'flex'
-    document.querySelector('.register').style.display = 'none'
-}
-else if (test == 2) {
-    document.querySelector('.login').style.display = 'none'
-    document.querySelector('.register').style.display = 'flex'
-}
-else {
-    document.querySelector('.login').style.display = 'none'
-    document.querySelector('.register').style.display = 'none'
-} 
-
+// depiction of error
 function DisplayError(errormsg) {
     document.querySelector('.error').style.display = 'flex'
     document.querySelector('.error-text span').textContent = errormsg
@@ -39,3 +51,20 @@ function DisplayError(errormsg) {
         document.querySelector('.error-text span').textContent = ''
     }, 2500);
 }
+
+// AUTH
+
+const logbtn = document.querySelector('.login-btn')
+
+logbtn.addEventListener('click', () => {
+    if(password.value == '') {
+        DisplayError('შეიყვანეთ პაროლი')
+    }
+    cef.emit('pwd:logpass', password.value)
+})
+
+
+// თუ პაროლი არასწორია პავნოდან უბრალოდ დააემიტე "pwd:wrongpass"
+cef.on('pwd:wrongpass', () => {
+    return DisplayError('პაროლი არასწორია')
+})
