a17dba55ca8169250699be3715ccda27df449d4e
Thmson <126760638+Kryzexx@users.noreply.github.com>
2025-05-12 14:52:45 +0400

    Update app.js

--- app.js
+++ app.js
@@ -1,78 +1,146 @@
-let password = document.querySelector("#passbar")
+let productsData = [
+    {
+        id: 0,
+        name: "მედიკამენტი",
+        image: "assets/images/meds.png",
+        price: 10,
+    },
+    {
+        name: "მელატონინი",
+        image: "assets/images/meds.png",
+        price: 5,
+    },
+    {
+        name: "სედატინი",
+        image: "assets/images/meds.png",
+        price: 7,
+    },
+    {
+        name: "ანალგინი",
+        image: "assets/images/meds.png",
+        price: 8,
+    },
+    {
+        name: "ნუროფენი",
+        image: "assets/images/meds.png",
+        price: 5,
+    },
+]
 
-document.querySelector('.eye-svg').addEventListener('click', () => {
-    if(password.type == 'password') {
-        password.type = 'text'
-    }
-    else { password.type = 'password' }
-})
+const productContainer = document.querySelector('.products-container')
 
-document.querySelector('.reg-btn').addEventListener('click', () => {
-    if(document.querySelector('#pass1').value.length < 6) {
-        return DisplayError('პაროლი უნდა იყოს მინიმუმ 6 ასო')
-    }
-    if(document.querySelector('#pass1').value != document.querySelector('#pass2').value) {
-        return DisplayError('პაროლები არ ემთხვევა')
-    }
-    if(document.querySelector('.inputmail').value.includes("@") == false) {
-        return DisplayError('არასწორი მეილი')
-    }
-    else {
-        // - success, ამას რო მიიღებ მერე უკვე სქესი და რეებიცაა, ამ CEF'ს destroybrowser-ი უქენი
-        cef.emit('data:pool:sucreg', document.querySelector('#pass1').value)
-    }
-})
+productsData.forEach((e, index) => {
 
-let authname = document.querySelector("#namebar1")
-cef.emit("data:pool:authname");
-cef.on("data:pool:authname", (newname) => {
-    authname.placeholder = `${newname}`
-})
-
-let regname = document.querySelector("#namebar2")
-cef.emit("data:pool:regname")
-cef.on("data:pool:regname", (newname) => {
-    regname.placeholder = `${newname}`
-})
-
-/* -- თუ უკვე რეგისტრირებულია, Response სახით დააბრუნე 1 ხოლო თუ არაა დააბრუნე 2;
-1 = Log In
-2 = Register */
-cef.emit("data:pool:auth");
-cef.on('data:pool:auth', (response) => { 
-    if(response == 1) {
-        document.querySelector('.login').style.display = 'flex'
-        document.querySelector('.register').style.display = 'none'
+    // filter product names. 
+    const splitName = e.name.split('')
+    if(splitName.length > 12) {
+        console.error("პროდუქტის სახელი არ შეიძლება იყოს 12 ასოზე მეტი")
+        return
     }
-    else {
-        document.querySelector('.login').style.display = 'none'
-        document.querySelector('.register').style.display = 'flex'
-    }
-}) 
+    e.id = index
+    const productDiv = document.createElement('div');
+    productDiv.classList.add('product');
 
-// depiction of error
-function DisplayError(errormsg) {
-    document.querySelector('.error').style.display = 'flex'
-    document.querySelector('.error-text span').textContent = errormsg
-    setTimeout(() => {
-        document.querySelector('.error').style.display = 'none'
-        document.querySelector('.error-text span').textContent = ''
-    }, 2500);
+    productDiv.innerHTML = `
+        <div class="product-img">
+            <img src="${e.image}" alt="${e.name}">
+        </div>
+        <div class="product-details">
+            <div class="product-name">
+                <span>${e.name}</span>
+                <div class="product-price">
+                    <img src="assets/images/pricetag.png" alt="pricetag">
+                    <span>${e.price}</span>
+                </div>
+            </div>
+            <div class="add-product" onclick="buyProduct(event)" data-id="${e.id}">
+                <img src="assets/images/add.png" alt="Add">
+            </div>
+        </div>
+    `;
+    productContainer.appendChild(productDiv)
+});
+
+async function buyProduct(e) {
+    const id = parseInt(e.currentTarget.getAttribute("data-id"))
+    const product = productsData.find(e => e.id == id)
+
+     const { value: quantity } = await Swal.fire({
+        title: "ჩაწერეთ პროდუქტის რაოდენობა",
+        input: "number",
+        inputLabel: "პროდუქტის რაოდენობა",
+        showCancelButton: true,
+        inputValidator: (value) => {
+          if (!value) {
+            return "გთხოვთ ჩაწეროთ პროდუქტის რაოდენობა";
+          }
+        }
+      });
+
+      if (quantity.isConfirmed) {
+        return 0;
+      }
+      let officialQuantity = Math.round(quantity)
+
+      if(officialQuantity && officialQuantity >= 1 || typeof(officialQuantity) == Number) {
+
+        if(officialQuantity > 50) {
+            return Swal.fire({
+                icon: "error",
+                title: "შეცდომა",
+                text: "პროდუქტების რაოდენობა არ შეიძლება იყოს 50 ზე მეტი",
+                scrollbarPadding: false,
+              });
+            }
+        }
+
+        let sum = Math.round(officialQuantity * product.price)
+
+        if(officialQuantity <= 0) {
+            return Swal.fire({
+                icon: "error",
+                title: "შეცდომა",
+                text: "პროდუქტების რაოდენობა არ შეიძლება იყოს 0 ან მასზე ნაკლები",
+                scrollbarPadding: false,
+              });
+        }
+        Swal.fire({
+            title: "დასტური",
+            text: `ნამდვილად გსურთ შეიძინოთ ${officialQuantity} ცალი ${product.name}, ${sum}$-ად?`,
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "დიახ",
+            scrollbarPadding: false,
+          }).then((result) => {
+            cef.on("pwd:getcash", (cash) => {
+             if (result.isConfirmed && cash >= sum) {
+              Swal.fire({
+                title: "Done",
+                text: `თქვენ შეიძინეთ ${officialQuantity} ცალი ${product.name}, ${sum}$-ად`,
+                icon: "success",
+                scrollbarPadding: false,
+              });
+              BuyProduct(product, sum, officialQuantity)
+              //cef.emit("pwd:buyitem",GasagzavniData)
+            }
+            else if(result.isConfirmed && cash < sum) {
+                Swal.fire({
+                    icon: "error",
+                    title: "შეცდომა",
+                    text: "თქვენ არ გაქვთ საკმარისი თანხა",
+                    scrollbarPadding: false,
+                  });
+                }
+            });
+        })
+    }
+
+cef.emit("onPlayerTest", "satesto vafvafi")
+function BuyProduct(product, sum, quantity) {
+    cef.emit("pwd:buyitem", product.name, sum, quantity) 
+    // აქ მოვა პროდუქტის სახელი (product.name),
+    // ჯამში რამდენი გადაიხადა (sum),
+    // და რამდენი იყიდა (quantity)
 }
-
-// AUTH
-
-const logbtn = document.querySelector('.login-btn')
-
-logbtn.addEventListener('click', () => {
-    if(password.value == '') {
-        DisplayError('შეიყვანეთ პაროლი')
-    }
-    cef.emit('data:pool:getpass', password.value)
-})
-
-
-// თუ პაროლი არასწორია პავნოდან უბრალოდ დააემიტე "data:pool:wrongpass"
-cef.on('data:pool:wrongpass', () => {
-    return DisplayError('პაროლი არასწორია')
-})
